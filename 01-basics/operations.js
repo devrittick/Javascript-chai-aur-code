0c04866d0c86940ecd71b6e65f0aace0a0546461
devrittick <devrittick@gmail.com>
2024-01-04 21:11:17 +0530

    learn operation

---
+++ 01-basics/operations.js
@@ -0,0 +1,24 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2 + 2);
+console.log(20 - 2);
+console.log(20 * 3);
+console.log(20 / 4);
+console.log(20 % 6);
+
+let str1 = "Hello";
+let str2 = "Rittick";
+
+console.log(str1 + str2);
+
+console.log("2" + 2);
+console.log(2 + "2");
+console.log("1" + 2 + 2);
+console.log("1" + 2 + "2");
+console.log(1 + 2 + "2");
+
+let num = 100;
+++num;
+console.log(num);
